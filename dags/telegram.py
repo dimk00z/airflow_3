573c6f8893e3aeda33b77ea329f8f94547eb3990
Dmitry <dimk00z@gmail.com>
2020-06-10 10:37:41 +0300

    AirtableOperator

--- dags/telegram.py
+++ dags/telegram.py
@@ -26,7 +26,7 @@ AIR_TABLE_BASE_KEY = os.getenv("AIR_TABLE_BASE_KEY")
 class TelegramOperator(BaseOperator):
     @apply_defaults
     def __init__(self, filename: str,
-                 chat_id_for_send='150399599',
+                 chat_id_for_send: str,
                  use_proxy=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
         env_path = Path('.') / '.env'
@@ -48,19 +48,20 @@ class TelegramOperator(BaseOperator):
         def callback_inline(call):
             if call.message:
                 if call.data == "test":
+                    new_message = "Спасибо! Ваш голос 2020 принят!"
                     bot.edit_message_text(
                         chat_id=call.message.chat.id,
                         message_id=call.message.message_id,
-                        text="Двинули!")
+                        text=new_message)
                     result_data_set = {
                         'chat_id': str(call.message.chat.id),
                         'username': call.from_user.username,
-                        'triggered_at': datetime.utcfromtimestamp(
+                        'triggered_at': datetime.fromtimestamp(
                             int(call.message.date)).strftime("%Y-%m-%dT%H:%M:%SZ"),
                         'reporter_name': 'dimk_smith',
-                        'event_type': 'event_type'
+                        'event_type': new_message
                     }
-                    with open(self.filename, 'w') as outfile:
+                    with open(self.filename, 'w', encoding='utf-8') as outfile:
                         json.dump(result_data_set, outfile)
                     bot.stop_polling()
                     print('Бот отработал')
@@ -70,7 +71,7 @@ class TelegramOperator(BaseOperator):
             text="Поехали", callback_data="test")
         keyboard.add(callback_button)
         bot.send_message(self.chat_id_for_send,
-                         'Dimk_smith', reply_markup=keyboard)
+                         'Карантин закончился, может в бар?', reply_markup=keyboard)
         bot.polling()
 
 
@@ -84,10 +85,36 @@ class FileCheckSensor(BaseSensorOperator):
         return os.path.isfile(self.filename)
 
 
+class AirtableOperator(BaseOperator):
+    @apply_defaults
+    def __init__(self, air_table: str, filename: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        env_path = Path('.') / '.env'
+        load_dotenv(dotenv_path=env_path)
+        self.api_key = os.getenv("AIR_TABLE_API_KEY")
+        self.base_key = os.getenv("AIR_TABLE_BASE_KEY")
+        self.air_table = air_table
+        self.filename = filename
+
+    def execute(self, *args, **kwargs):
+        with open(self.filename, encoding='utf-8') as f:
+            data_set = json.load(f)
+        airtable = Airtable(self.base_key, self.air_table,
+                            api_key=self.api_key)
+        records = airtable.get_all()
+        airtable.insert({key: str(data_set[key]) for key in data_set})
+
+
 default_args = {
     'owner': 'Dimk_smith',
     'start_date': days_ago(2),
+    # test
+    # 'chat_id_for_send': '150399599',
+    # 'air_table': 'air_table',
+
+    # prod
     'chat_id_for_send': '-496351002',
+    'air_table': 'airflow101',
 
     'filename': '/tmp/temp.json'}
 
@@ -100,6 +127,8 @@ send_telegram_message = TelegramOperator(
 wait_for_file = FileCheckSensor(
     task_id='wait_for_file', poke_interval=10, dag=dag)
 
+write_to_airtable = AirtableOperator(
+    task_id='write_to_airtable', dag=dag)
 
 all_success = DummyOperator(task_id='all_success', dag=dag)
-send_telegram_message >> wait_for_file >> all_success
+send_telegram_message >> wait_for_file >> write_to_airtable >> all_success
